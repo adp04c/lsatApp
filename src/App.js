4da855ffbc5e3fd4ff172758159e8073acc31753
rvino <rapulvino@gmail.com>
2017-08-09 22:25:28 -0700

    Added a line to the html as a test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Rory's and A2s sick app for law students who want to perform well and learn to do other things</h2>
+          <h3>We have a lot to learn to get this to work</h3>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
